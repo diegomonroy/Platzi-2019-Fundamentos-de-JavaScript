eac1056212ac61513f0ab31f8801dbbca1ce4858
Diego Fernando Monroy Muñoz <dmsoftwaresas@gmail.com>
2019-01-22 03:02:40 +0000

    Variables.

---
+++ clase1.js
@@ -0,0 +1,11 @@
+// console.log('Hola mundo JS!')
+
+var nombre = 'Diego', apellido = 'Monroy'
+var edad = 32
+
+edad = '32 años'
+
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad)
+
+var peso = 75
